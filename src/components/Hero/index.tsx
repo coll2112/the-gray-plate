76ba1516ccd62f20265a42ad5e28cbe8eec3460f
Gray754 <lgc9312@gmail.com>
2021-02-13 16:04:42 -0600

    added hero to the components

---
+++ src/components/Hero/index.tsx
@@ -0,0 +1,33 @@
+import React, { FunctionComponent } from 'react'
+import Button from '~/components/Button'
+import styles from './hero.module.scss'
+
+interface Props {
+	title: string
+	subtitle?: string
+	bgImg?: string
+	href: string
+	btnText: string
+	[rest: string]: unknown // ...rest property
+}
+
+const Hero: FunctionComponent<Props> = ({
+	title,
+	subtitle,
+	href,
+	bgImg,
+	btnText,
+	...rest
+}) => {
+	return (
+		<div className={styles.container} {...rest}>
+			<h1 className={styles.title}>{title}</h1>
+			<p className={styles.subtitle}>{subtitle}</p>
+			<Button type='secondary' href={href}>
+				{btnText}
+			</Button>
+		</div>
+	)
+}
+
+export default Hero
