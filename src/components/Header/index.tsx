84482eb384f6401ea44105f353fba30e98ec829b
coll2112 <lgc9312@gmail.com>
2021-05-08 04:49:35 -0500

    set active link in header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,28 +1,41 @@
-import React, { FunctionComponent } from 'react'
+import React, { FunctionComponent, useMemo, useState } from 'react'
+import { useRouter } from 'next/router'
 import Link from 'next/link'
 
+import clsx from 'clsx'
 import styles from './header.module.scss'
 
 const Header: FunctionComponent = () => {
+  const [linkPathName, setLinkPathName] = useState<string>()
+  const { route } = useRouter()
   const links = [
     { name: 'Home', href: '/' },
     { name: 'About', href: '/about' },
     { name: 'Projects', href: '/projects' }
   ]
 
+  useMemo(() => {
+    setLinkPathName(route)
+  }, [route])
+
+  console.log(linkPathName)
+
   return (
     <div className={styles.container}>
       <h3 className={styles.title}>Web Development</h3>
       <div className={styles.links}>
-        {links.map(({ href, name }) => (
-          <Link key={name} href={href}>
-            <a className={styles.link}>
-              <span className={styles.btnSides} />
-              {/* TODO: Fix the buttons jumpiness */}
-              <p className={styles['link-text']}>{name}</p>
-            </a>
-          </Link>
-        ))}
+        {links.map(({ href, name }) => {
+          const activeLink = linkPathName === href && styles.active
+          return (
+            <Link key={name} href={href}>
+              <a className={clsx(styles.link, activeLink)}>
+                <span className={clsx(styles.btnSides, activeLink)} />
+                {/* TODO: Fix the buttons jumpiness */}
+                <p className={styles['link-text']}>{name}</p>
+              </a>
+            </Link>
+          )
+        })}
       </div>
     </div>
   )
