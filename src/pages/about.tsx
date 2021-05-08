e1f5a94892681839439e4ab790715f3da44f6d06
coll2112 <lgc9312@gmail.com>
2021-05-08 04:54:23 -0500

    updated some stuff

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Button from 'components/Button'
+import Header from 'components/Header'
 
 export default function About() {
   return (
     <div>
+      <Header />
       <h1>About Page</h1>
       <div>
         <Button href="/" type="secondary">
