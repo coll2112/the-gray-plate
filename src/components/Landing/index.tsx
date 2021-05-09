5b15ac6c5d8cfa52955fe0c91d53cc4e2b1523ba
coll2112 <lgc9312@gmail.com>
2021-05-09 03:01:23 -0500

    created text animation

--- src/components/Landing/index.tsx
+++ src/components/Landing/index.tsx
@@ -5,6 +5,15 @@ import styles from './landing.module.scss'
 const Landing: FunctionComponent = () => (
   <div className={styles.container}>
     <h1 className={styles.title}>Front End Web Developer</h1>
+    <h2>
+      I am a
+      <div className={styles.words}>
+        <span>Front End Web Developer</span>
+        <span>Loving Husband</span>
+        <span>Lover of Dogs</span>
+        <span>Super Comic Book Geek</span>
+      </div>
+    </h2>
     <span className={styles.backgroundImg} />
     <span className={styles.backgroundColor} />
   </div>
