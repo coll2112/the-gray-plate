3b45333dea4b5237d19e7662f5c306ddd98ccd2d
Gray754 <lgc9312@gmail.com>
2021-05-16 00:47:33 -0500

    cleaned up components

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,8 @@
 import React, { FunctionComponent, useMemo, useState } from 'react'
 import { useRouter } from 'next/router'
 import Link from 'next/link'
-
 import clsx from 'clsx'
+
 import styles from './header.module.scss'
 
 const Header: FunctionComponent = () => {
@@ -19,22 +19,22 @@ const Header: FunctionComponent = () => {
     setLinkPathName(route)
   }, [route])
 
+  const linkMap = links.map(({ href, name }) => {
+    const activeLink = linkPathName === href && styles.active
+    return (
+      <Link key={name} href={href}>
+        <a className={clsx(styles.link, activeLink)}>
+          <span className={clsx(styles.btnSides, activeLink)} />
+          <p className={styles['link-text']}>{name}</p>
+        </a>
+      </Link>
+    )
+  })
+
   return (
     <div className={styles.container}>
-      <h3 className={styles.title}>Web Development</h3>
-      <div className={styles.links}>
-        {links.map(({ href, name }) => {
-          const activeLink = linkPathName === href && styles.active
-          return (
-            <Link key={name} href={href}>
-              <a className={clsx(styles.link, activeLink)}>
-                <span className={clsx(styles.btnSides, activeLink)} />
-                <p className={styles['link-text']}>{name}</p>
-              </a>
-            </Link>
-          )
-        })}
-      </div>
+      <h3 className={styles.title}>Lee Collins</h3>
+      <div className={styles.links}>{linkMap}</div>
     </div>
   )
 }
--- src/components/Landing/index.tsx
+++ src/components/Landing/index.tsx
@@ -1,23 +1,15 @@
-import SlidingText from 'components/SlidingText'
 import React, { FunctionComponent } from 'react'
 
 import styles from './landing.module.scss'
 
-const Landing: FunctionComponent = () => {
-  const items = ['Developer', 'Husband', 'Dog Lover', 'Geek']
-
-  return (
-    <div className={styles.container}>
-      <p className={styles.preTitle}>Lee Collins</p>
+const Landing: FunctionComponent = () => (
+  <div className={styles.container}>
+    <div className={styles.innerContent}>
       <h1 className={styles.title}>Front End Web Developer</h1>
-      <div className={styles.text}>
-        <h4>Meet Lee! He's a...</h4>
-        <SlidingText words={items} />
-      </div>
-      <span className={styles.backgroundImg} />
-      <span className={styles.backgroundColor} />
     </div>
-  )
-}
+    <span className={styles.backgroundImg} />
+    <span className={styles.backgroundColor} />
+  </div>
+)
 
 export default Landing
