b84760d716a523081bc1a283f9225365198b84f2
Gray754 <lgc9312@gmail.com>
2021-05-16 04:52:07 -0500

    changed up structure for scss files

--- src/components/Landing/index.tsx
+++ src/components/Landing/index.tsx
@@ -3,13 +3,13 @@ import React, { FunctionComponent } from 'react'
 import styles from './landing.module.scss'
 
 const Landing: FunctionComponent = () => (
-  <div className={styles.container}>
-    <div className={styles.innerContent}>
+  <>
+    <div className={styles.container}>
       <h1 className={styles.title}>Front End Web Developer</h1>
+      <span className={styles.backgroundImg} />
+      <span className={styles.backgroundColor} />
     </div>
-    <span className={styles.backgroundImg} />
-    <span className={styles.backgroundColor} />
-  </div>
+  </>
 )
 
 export default Landing
